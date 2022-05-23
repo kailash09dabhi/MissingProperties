dea88bf67db37aa8f390aee7187c1ff7ba30ee34
Kailas Dabhi <v-kailas.dabhi@decurtis.com>
2022-05-23 20:05:47 +0530

    Set default empty object or array in patched object for first missing node

--- index.js
+++ index.js
@@ -41,6 +41,7 @@ const getMissingDataInfo = ({object, elements, path, defaultValue,defaultValuePa
   }
   // traversal in progress
   if(!data) {
+    set(object, newPath, firstElement.isArray ? [] : {})
     return { missingPath:newPath }
   }
 
@@ -83,42 +84,42 @@ const getMissingDataInfoWithPatchedObject = (object, paths) => {
 
 export default getMissingDataInfoWithPatchedObject
 
-// // Example USAGE
-// export const response = {
-//   articles: {
-//     page: {
-//       details: {
-//           // description: 'This is description',
-//           totalChar: 199,
-//       },
-//     },
-//   },
-//   animals: [
-//     {
-//       name: 'Dogs',
-//       breeds: [
-//         // { name: 'Doggy One', age: 10, location: 'panama' },
-//         // { name: 'Doggy Two', age: 20, location: 'america' },
-//         // { age: 30, location: 'cyprus' },
-//       ],
-//     },
-//     {
-//       name: 'Bird',
-//       breeds: [
-//         { age: 33, location: 'bulgaria' },
-//         { name: 'Bird One', age: 11, location: 'india' },
-//         { name: 'Bird Two', age: 22, location: 'japan' },
-//       ],
-//     },
-//   ],
-//   friends: [
-//     // { user: '', age: 36, active: true },
-//     // { user: 'fred', age: 40, active: false },
-//     // { age: 37, active: true },
-//   ],
-// }
+// Example USAGE
+export const response = {
+  // articles: {
+  //   // page: {
+  //   //   details: {
+  //   //       // description: 'This is description',
+  //   //       totalChar: 199,
+  //   //   },
+  //   // },
+  // },
+  animals: [
+    {
+      name: 'Dogs',
+      breeds: [
+        // { name: 'Doggy One', age: 10, location: 'panama' },
+        // { name: 'Doggy Two', age: 20, location: 'america' },
+        // { age: 30, location: 'cyprus' },
+      ],
+    },
+    {
+      name: 'Bird',
+      breeds: [
+        { age: 33, location: 'bulgaria' },
+        { name: 'Bird One', age: 11, location: 'india' },
+        { name: 'Bird Two', age: 22, location: 'japan' },
+      ],
+    },
+  ],
+  friends: [
+    // { user: '', age: 36, active: true },
+    // { user: 'fred', age: 40, active: false },
+    // { age: 37, active: true },
+  ],
+}
 
-// getMissingDataInfoWithPatchedObject(response,[
-//   {path: 'animals[].breeds[].name',defaultValue:'INTELLIGENT ANIMAL',defaultValuePath:'animals[].breeds[].age',logKeyPath:'animals[].breeds[].location'},
-//   {path: 'articles.page.details.description',defaultValue:'INDIA',defaultValuePath:'articles.page.details.totalChar',logKeyPath:'articles.page.details.totalChar'},
-// ])
+console.log(JSON.stringify(getMissingDataInfoWithPatchedObject(response,[
+  {path: 'animals[].breeds[].name',defaultValue:'INTELLIGENT ANIMAL',defaultValuePath:'animals[].breeds[].age',logKeyPath:'animals[].breeds[].location'},
+  {path: 'articles.page.details.description',defaultValue:'INDIA',defaultValuePath:'articles.page.details.totalChar',logKeyPath:'articles.page.details.totalChar'},
+])))
